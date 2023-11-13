a6f2657ce42e02f9b4f31000a1b8d434ac0cb793
Gildas <72899528+achmadgildas@users.noreply.github.com>
2023-11-13 23:39:14 +0800

    find even number from 1 to 100

---
+++ evenNumber100.js
@@ -0,0 +1,11 @@
+let evenNumber = [];
+
+for (let i = 1; i <= 100; i++) {
+        if (i % 2 === 0) {
+        evenNumber.push(i);
+    }
+    
+}
+
+console.log("Bilangan genap dari 1 hingga 100:");
+console.log(evenNumber);
